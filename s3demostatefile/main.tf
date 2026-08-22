327016a95113c79736413feb9a95e9494930547f
CloudDevOpsHub <100401787+CloudDevOpsHub@users.noreply.github.com>
2026-08-22 09:17:41 +0530

    Update main.tf

--- s3demostatefile/main.tf
+++ s3demostatefile/main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
 resource "aws_s3_bucket" "demo" {
   bucket = "vikas-terraform-demo-12345"
 }
